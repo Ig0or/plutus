4054407cbceae0c7b76931d2173ac745bcc36982
Ig0or <igorprovensi@gmail.com>
2023-03-08 17:14:09 -0300

    feat: figlet on main file

--- plutus.py
+++ plutus.py
@@ -1,14 +1,30 @@
 # Third Party
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pyfiglet import print_figlet
 import loglifos
 import uvicorn
 
 # Local
 from src.routers.base.base_router import BaseRouter
 
-app = BaseRouter.register_routers()
-
-
 loglifos.set_config(log_level=loglifos.WARNING)
 
+
+def build_app() -> FastAPI:
+    app = BaseRouter.register_routers()
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
+
+
+app = build_app()
+
 if __name__ == "__main__":
+    print_figlet(text="Plutus", colors="76;0;153")
     uvicorn.run(app=app)
