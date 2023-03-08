08e60afd15e173ec335c6cc5035ec45c88f2066d
Ig0or <igorprovensi@gmail.com>
2023-03-08 13:40:20 -0300

    feat: method to save new credit card

--- src/repositories/credit_card/credit_card_repository.py
+++ src/repositories/credit_card/credit_card_repository.py
@@ -1,4 +1,8 @@
 # Local
+from typing import NoReturn
+
+# Local
+from src.domain.models.credit_card.credit_card_model import CreditCardModel
 from src.infrastructure.mongodb.mongodb_infrastructure import MongoDBInfrastructure
 
 
@@ -7,7 +11,15 @@ class CreditCardRepository:
     async def get_all_credit_cards() -> list[dict]:
         connection = MongoDBInfrastructure.get_connection()
 
-        credit_cards = connection.find()
+        credit_cards = connection.find({}, {"_id": 0})
         credit_cards_list = list(credit_cards)
 
         return credit_cards_list
+
+    @staticmethod
+    async def save_new_credit_card(credit_card_model: CreditCardModel) -> NoReturn:
+        connection = MongoDBInfrastructure.get_connection()
+
+        connection.insert_one(document=credit_card_model)
+
+        return
