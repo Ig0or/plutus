2020adcf9ae3ed15547294dc3346d1b3312a2ed6
Ig0or <igorprovensi@gmail.com>
2023-03-09 11:11:10 -0300

    test: using mocks to fix tests

--- tests/services/obfuscate_data/test_obfuscate_data_service.py
+++ tests/services/obfuscate_data/test_obfuscate_data_service.py
@@ -1,16 +1,23 @@
+# Standard
+from unittest.mock import patch
+
 # Third Party
+from cryptography.fernet import Fernet
 import pytest
 
 # Local
 from src.services.obfuscate_data.obfuscate_data_service import ObfuscateDataService
 from tests.services.obfuscate_data.stubs import (
+    encoded_decrypted_stub,
+    encoded_encrypted_stub,
     invalid_type_number_stub,
     valid_encrypted_stub,
     valid_type_number_stub,
 )
 
 
-def test_obfuscate_value_when_value_is_valid_then_return_encrypted_string():
+@patch.object(Fernet, "encrypt", return_value=encoded_encrypted_stub)
+def test_obfuscate_value_when_value_is_valid_then_return_encrypted_string(fernet_mock):
     encrypted_value = ObfuscateDataService.obfuscate_value(value=valid_type_number_stub)
 
     assert len(encrypted_value) == 120
@@ -22,7 +29,10 @@ def test_obfuscate_value_when_value_is_invalid_then_raise_attribute_error():
         ObfuscateDataService.obfuscate_value(value=invalid_type_number_stub)
 
 
-def test_deobfuscate_value_when_value_is_valid_then_return_decrypted_string():
+@patch.object(Fernet, "decrypt", return_value=encoded_decrypted_stub)
+def test_deobfuscate_value_when_value_is_valid_then_return_decrypted_string(
+    fernet_mock,
+):
     decrypted_value = ObfuscateDataService.deobfuscate_value(value=valid_encrypted_stub)
 
     assert decrypted_value == valid_type_number_stub
